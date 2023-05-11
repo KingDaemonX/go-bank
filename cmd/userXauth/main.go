3679cd72f0c5d0c4faa47c9d6e73e6eeb8adbef5
KingAnointig <adetoyeanointing15@gmail.com>
2023-05-11 19:54:47 +0100

    add : cors header middleware function

--- cmd/userXauth/main.go
+++ cmd/userXauth/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/KingDaemonX/evolve-mod-ddd-sample/adapt"
+	"github.com/KingDaemonX/evolve-mod-ddd-sample/services/users/application"
+	"github.com/KingDaemonX/evolve-mod-ddd-sample/services/users/interfaces"
 )
+
+func init() {
+	// load .env variable here
+}
 
 func main() {
 	fmt.Println("Hello World From KingDaemonX 👋 \nI am building a bank application")
+
+	services, err := adapt.ConnectDatabase()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	user := interfaces.NewUserInterface((*application.UserAppInterface)(&services.UserInfra))
+
+	
 }
