c4b6067b3d24d873e729f6732144d5b441b2f859
KingAnointig <adetoyeanointing15@gmail.com>
2023-05-13 13:14:52 +0100

    add : account application layer to connect domain and interface

--- services/accounts/application/accountApp.go
+++ services/accounts/application/accountApp.go
@@ -1,1 +1,30 @@
 package application
+
+import (
+	"github.com/KingDaemonX/evolve-mod-ddd-sample/services/accounts/domain/entity"
+	"github.com/KingDaemonX/evolve-mod-ddd-sample/services/accounts/domain/repository"
+)
+
+type accountApp struct {
+	account repository.AcccountRepository
+}
+
+type AccountApplication interface {
+	CreateAccount(*entity.Account) (any, error)
+	ReadAccountByAccountNumber(string) (any, error)
+	UpdateAccountBalance(*entity.UpdateBalance) (any, error)
+}
+
+var _ repository.AcccountRepository = &accountApp{}
+
+func (aa *accountApp) CreateAccount(account *entity.Account) (any, error) {
+	return aa.account.CreateAccount(account)
+}
+
+func (aa *accountApp) ReadAccountByAccountNumber(accountNumber string) (any, error) {
+	return aa.account.ReadAccountByAccountNumber(accountNumber)
+}
+
+func (aa *accountApp) UpdateAccountBalance(fundsInfo *entity.UpdateBalance) (any, error) {
+	return aa.account.UpdateAccountBalance(fundsInfo)
+}
